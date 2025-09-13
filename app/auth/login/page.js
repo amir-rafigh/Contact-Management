3084b697d40d55ef81d3a8b47ce09a25b6b1e12c
amir_rafigh <amir.rafigh.work@gmail.com>
2025-09-13 13:59:24 +0330

    finish the design login page 09/13 02:00pm

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,46 +1,116 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Login() {
+  const [loginmethod, setLoginmethod] = useState(false);
+  const [logincode, setLogincode] = useState(false);
+
   return (
     <div className="login_container flex justify-center bg-gradient-to-b from-[#5222d0] to-transparent items-center gap-5 w-full h-full">
 
-      <div className="mobile_container w-full">
+
+      <div className="mobile_container w-full md:w-lg lg:w-2xl">
 
         <div className="title text-center lalezar text-4xl">
           <h1>ورود به پنل کاربری</h1>
         </div>
 
         <div className="login_method flex justify-center gap-5 my-4 px-4">
-          <button className="bg-[#5222d0] text-white p-2 rounded-lg">ورود با نام کاربری</button>
-          <button className="">ورود با شماره موبایل</button>
+
+          <button
+            onClick={() => setLoginmethod(false)}
+            className={
+              loginmethod ? "" : `bg-[#5222d0] text-white p-2 rounded-lg`
+            }
+          >
+            ورود با نام کاربری
+          </button>
+
+          <button
+            onClick={() => setLoginmethod(true)}
+            className={
+              loginmethod ? `bg-[#5222d0] text-white p-2 rounded-lg` : ""
+            }
+          >
+            ورود با شماره موبایل
+          </button>
         </div>
 
         <form action="" className="fieldset rounded-box p-4">
-
-            <label className="label t">ایمیل</label>
-            <input type="email" className="input w-full py-6" name="email" placeholder="ایمیل"  />
-            <label className="label">رمز عبور</label>
-            <input type="password" className="input w-full py-6" name="password" placeholder="رمز عبور"/>
-
-            <button className="p-2 rounded-2xl bg-[#5222d0] text-white text-lg ">ورود </button>
-            <Link href="/auth/register">
-              <p className="text-center text-[#5222d0]">هنوز ثبت نام نکرده اید ؟</p>
-            </Link>
-          
+          {loginmethod ? (
+            <>
+              {logincode ? (
+                <>
+                  <label className="label">کد تایید :</label>
+                  <input
+                    type="number"
+                    className="input w-full py-6 md:w-lg lg:w-2xl"
+                    name="phoneNumber"
+                    placeholder="کد 4 رقمی"
+                  />
+                  <button className="p-2 rounded-2xl bg-[#5222d0] text-white text-lg md:w-lg lg:w-2xl">
+                    ورود
+                  </button>
+                </>
+              ) : (
+                <>
+                  <label className="label">شماره موبایل :</label>
+                  <input
+                    type="number"
+                    className="input w-full py-6 md:w-lg lg:w-2xl"
+                    name="phoneNumber"
+                    placeholder="شماره موبایل"
+                  />
+                  <button
+                    onClick={() => setLogincode(true)}
+                    className="p-2 rounded-2xl bg-[#5222d0] text-white text-lg md:w-lg lg:w-2xl"
+                  >
+                    دریافت کد
+                  </button>
+                </>
+              )}
+            </>
+          ) : (
+            <>
+              <label className="label t">ایمیل</label>
+              <input
+                type="email"
+                className="input w-full py-6 md:w-lg lg:w-2xl"
+                name="email"
+                placeholder="ایمیل"
+              />
+              <label className="label">رمز عبور</label>
+              <input
+                type="password"
+                className="input w-full py-6 md:w-lg lg:w-2xl"
+                name="password"
+                placeholder="رمز عبور"
+              />
+              <button className="p-2 rounded-2xl bg-[#5222d0] text-white text-lg md:w-lg lg:w-2xl">
+                ورود{" "}
+              </button>
+              <Link href="/auth/register">
+                <p className="text-center text-[#5222d0]">
+                  هنوز ثبت نام نکرده اید ؟
+                </p>
+              </Link>
+            </>
+          )}
+        
         </form>
-
       </div>
-      <div className="desktop_container hidden sm:block">
+
+      <div className="desktop_container hidden md:block">
         <Image
-        src="/image/undraw_access-account_aydp (2).svg"
-        width={500}
-        height={500}
-        objectFit="cover"
-        alt="login image"
+          src="/image/undraw_access-account_aydp (2).svg"
+          width={700}
+          height={700}
+          objectFit="cover"
+          alt="login image"
         />
       </div>
-
     </div>
   );
 }
--- app/auth/register/page.js
+++ app/auth/register/page.js
@@ -24,35 +24,35 @@ export default function Register() {
 
 
   return (
-    <div className="register_container bg-gradient-to-b from-[#5222d0] to-transparent flex  justify-center align-items-center h-full w-full">
+    <div className="register_container bg-gradient-to-b from-[#5222d0] to-transparent flex  justify-center items-center h-full w-full">
 
-      <div className="mobile_container flex flex-col h-full justify-center gap-5">
+      <div className="mobile_container flex flex-col  w-full md:w-md lg:w-xl justify-center items-center gap-5 p-4">
         <div className="title text-center lalezar text-4xl">
           <h1>مدیریت مخاطب های شما</h1>
         </div>
 
-        <form action={formAction} className="fieldset bg-base-200 border-[#3f3d58]  rounded-box w-xs border-2 p-4">
+        <form action={formAction} className="fieldset bg-base-200 border-[#3f3d58]  items-center  rounded-box w-full  border-2 p-4">
                
 
             <legend className="fieldset-legend text-3xl">ثبت نام</legend>
 
             <label className="label">نام</label>
-            <input type="text" className="input" name="firstName" placeholder="نام" defaultValue={state.values?.firstName||""}/>
+            <input type="text" className="input  w-full" name="firstName" placeholder="نام" defaultValue={state.values?.firstName||""}/>
 
             <label className="label">نام خانوادگی</label>
-            <input type="text" className="input" name="lastName" placeholder="نام خانوادگی" defaultValue={state.values?.lastName || ""} />
+            <input type="text" className="input  w-full" name="lastName" placeholder="نام خانوادگی" defaultValue={state.values?.lastName || ""} />
 
             <label className="label">ایمیل</label>
-            <input type="email" className="input" name="email" placeholder="ایمیل" defaultValue={state.values?.email||""} />
+            <input type="email" className="input  w-full" name="email" placeholder="ایمیل" defaultValue={state.values?.email||""} />
 
             <label className="label">شماره موبایل</label>
-            <input type="number" className="input" name="phoneNumber" placeholder="شماره موبایل" defaultValue={state.values?.phoneNumber||""}/>
+            <input type="number" className="input  w-full" name="phoneNumber" placeholder="شماره موبایل" defaultValue={state.values?.phoneNumber||""}/>
 
             <label className="label">رمز عبور</label>
-            <input type="password" className="input" name="password" placeholder="رمز عبور" defaultValue={state.values?.password||""}/>
+            <input type="password" className="input  w-full" name="password" placeholder="رمز عبور" defaultValue={state.values?.password||""}/>
 
             <label className="label"> تکرار رمز عبور</label>
-            <input type="password" className="input" name="rePassword" placeholder="تکرار رمز عبور" defaultValue={state.values?.rePassword||""}/>
+            <input type="password" className="input  w-full" name="rePassword" placeholder="تکرار رمز عبور" defaultValue={state.values?.rePassword||""}/>
 
             <Submisson/>
             <p className="text-center">قبلا ثبت نام انجام داده اید ؟ <Link href="/auth/login"><span className="text-[#5222d0]  text-sm">وارد شوید</span></Link></p>
@@ -62,11 +62,11 @@ export default function Register() {
 
 
       
-      <div className="desktop_container hidden sm:block sm:flex">
+      <div className="desktop_container hidden md:block md:flex">
             <Image 
             src="/image/undraw_sign-up_z2ku.svg"
-            width={500}
-            height={500}
+            width={700}
+            height={700}
             objectFit="cover"
             alt="register image"
             />
